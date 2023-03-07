fb534c5fcfa2a1c4a8a5727c9040505138c48acf
George Halterman <george.h881@gmail.com>
2023-03-06 19:24:52 -0800

    Small update to remove extra import statements

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from "react-dom";
 
 import ReviewList from './reviews/body/reviewList.jsx'

4dcedf5b6e06a7172561c24d7e3bf359d9e8a418
George Halterman <george.h881@gmail.com>
2023-03-08 11:57:32 -0800

    Getting ready for a merge

--- client/src/reviews/body/individualReview.jsx
+++ client/src/reviews/body/individualReview.jsx
@@ -10,8 +10,8 @@ const IndividualReview = () => {
         This is from the IndividualReview
       </div>
       <div className="star" style={{ position: 'relative', fontSize: '25px' }}>
-        <FontAwesomeIcon icon={regularStar} style={{ position: 'absolute', left: 0, top: 0, clipPath: 'inset(0 ' + (-40) + '% 0 0)', color: '#757575' }} />
-        <FontAwesomeIcon icon={solidStar} style={{ position: 'absolute', left: 0, top: 0, clipPath: 'inset(0 ' + (100-60) + '% 0 0)', color: '#757575' }} />
+        <FontAwesomeIcon icon={regularStar} style={{ position: 'absolute', left: 0, top: 0, clipPath: 'inset(0 ' + (-60) + '% 0 0)', color: '#757575' }} />
+        <FontAwesomeIcon icon={solidStar} style={{ position: 'absolute', left: 0, top: 0, clipPath: 'inset(0 ' + (100-40) + '% 0 0)', color: '#757575' }} />
         <br />
       </div>
     </>

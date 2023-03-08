67c36b46be4106d26e42ae5993183ad56e32a74a
Addie Lopshire-Bratt <bratt.addie@gmail.com>
2023-03-08 08:53:42 -0600

    added questions list to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import ProductOverview from './product/ProductOverview.jsx'
 import ReviewList from './reviews/body/reviewList.jsx'
 import ProductBreakdown from './reviews/summary/productBreakdown.jsx'
+import QuestionsList from './questions/QuestionsList.jsx'
 
 const App = () => {
 
@@ -13,6 +14,7 @@ const App = () => {
       <ProductOverview />
       <ReviewList />
       <ProductBreakdown />
+      <QuestionsList />
     </div>
 
   )
---
+++ client/src/questions/QuestionsList.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+const QuestionsList = () => {
+  return (
+    <section className='list questionsList questionsList--extended'>
+      <div>
+        This is the Questions List
+      </div>
+    </section>
+  )
+}
+
+
+
+
+
+
+
+export default QuestionsList;

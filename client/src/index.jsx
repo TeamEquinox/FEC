87848749352978ad366542f940078c1cfaa25980
kyle <kyle.dick0811@gmail.com>
2023-03-07 17:13:00 -0700

    added some of Georges code

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,9 +3,14 @@ import ReactDom from "react-dom";
 import { useState, useEffect } from 'react';
 import Parent from './related/Parent.jsx';
 
+import ReviewList from './reviews/body/reviewList.jsx'
+import ProductBreakdown from './reviews/summary/productBreakdown.jsx'
+
 const App = () => {
   return (
     <div>
+      <ReviewList />
+      <ProductBreakdown />
       <Parent/>
     </div>
   )

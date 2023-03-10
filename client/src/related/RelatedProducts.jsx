7e20e264ff5da44c20f59aa6317b309d84f5addf
kyle <kyle.dick0811@gmail.com>
2023-03-10 11:50:59 -0700

    added api request to client

--- client/src/related/RelatedProducts.jsx
+++ client/src/related/RelatedProducts.jsx
@@ -5,11 +5,23 @@ import Outfits from './Outfits.jsx';
 
 const RelatedProducts = ({ product }) => {
   console.log('inside RelatedProducts ', product);
-
   
+  //managing state==========================
+  const [relatedData, setRelatedData] = useState([]);
+
+  //API requests============================
+  var getRelatedProducts = () => {
+    axios.get('relatedProducts', {params: {data: id}})
+      .then((data) => {
+        console.log('recieved data in the client getRelatedProducts get request', data)
+      })
+      .catch((err) => console.log('There was an error in the getRelatedProducts get request: ', err))
+  }
+
+  //returning components to index.jsx=======
   return (
     <div>
-     <Product />
+     <Product relatedData={relatedData}/>
      <Outfits/>
     </div>
   )

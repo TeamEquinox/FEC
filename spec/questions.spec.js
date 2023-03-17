c98e48a879c08ae3e3510a1837c4e761edc86305
George Halterman <george.h881@gmail.com>
2023-03-16 20:03:17 -0700

    Adding tests and fixing small bugs

--- spec/questions.spec.js
+++ spec/questions.spec.js
@@ -5,5 +5,5 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import { toBeInTheDocument } from '@testing-library/jest-dom';
-import RelatedProducts from './client/src/related/RelatedProducts.jsx';
+import RelatedProducts from '../client/src/related/RelatedProducts.jsx';
 import product from './mochData.js';
--- spec/relatedProducts.spec.js
+++ spec/relatedProducts.spec.js
@@ -3,12 +3,12 @@
  */
 
 import * as React from 'react';
-import { render, screen } from '@testing-library/react'; 
+import { render, screen } from '@testing-library/react';
 import { toBeInTheDocument } from '@testing-library/jest-dom';
-import RelatedProducts from './client/src/related/RelatedProducts.jsx'; 
+import RelatedProducts from '../client/src/related/RelatedProducts.jsx';
 import product from './mochData.js';
 
-/* 
+/*
 A "describe" block can be used to group together multiple tests
 which check the same nodule or function.
  */
@@ -24,7 +24,7 @@ describe("Example tests", function(){
     */
     expect(add(1,1)).toBe(2);
   });
-  
+
   // In addition to expected, "happy path", behaviour as above, you should also test your edge cases
   it("Should return Infinity for numbers of type Number which are very large", function(){
     expect(add(1.6E310, 1)).toBe(Infinity);
@@ -52,7 +52,7 @@ test('renders component', () => {
 })
 
 describe('RelatedProduct', () => {
-  
+
   it('Displays the related products component', () => {
     render(<RelatedProducts product={product}/>);
     expect(screen.getByText(/Category/)).toBeInTheDocument();
@@ -63,7 +63,7 @@ describe('RelatedProduct', () => {
     render(<RelatedProducts product={product}/>)
     expect(screen.getByText(/Name/)).toBeInTheDocument();
   })
-  
+
   it('renders component', () => {
     render(<RelatedProducts product={product}/>)
     expect(screen.getByText(/Price/)).toBeInTheDocument();

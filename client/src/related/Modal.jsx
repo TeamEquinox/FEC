ad0147b857367cda5047a0ed2442e558136b5eb1
kyle <kyle.dick0811@gmail.com>
2023-03-18 15:09:08 -0600

    completed basic functionallity of pop up comparison modal

--- client/src/related/Modal.jsx
+++ client/src/related/Modal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { HiCheck } from 'react-icons/Hi';
 const Modal = ({ setShowModal, compares, setDataToCompare }) => {
   console.log('COMPARE--------------->', compares);
  
@@ -26,16 +26,38 @@ const Modal = ({ setShowModal, compares, setDataToCompare }) => {
     }
 
     for (var key in obj1) {
+      let bool1 = obj1[key];
+      let bool2 = obj2[key];
       if (obj2[key]) {
-        foundInBoth.push([obj1[key], key, obj2[key]]);
+      
+        if (bool1 === true) {
+          bool1 = <HiCheck/>;
+        }
+        if (bool1 === false) {
+          bool1 = '';
+        }
+        if (bool2 === true) {
+          bool2 = <HiCheck/>;
+        } 
+        if (bool2 === false) {
+          bool1 = '';
+        }
+        foundInBoth.push([bool1, key, bool2]);
         delete obj2[key];
       } else {
-        foundInFirst.push([obj1[key], key, ''])
+        foundInFirst.push([bool1, key, ''])
       }
     }
     if (findLongest(obj2) > 0) {
       for (var key in obj2) {
-        foundInSecond.push(['', key, obj2[key]]);
+        let bool2 = obj2[key];
+        if (bool2 === true) {
+          bool2 = <HiCheck/>;
+        } 
+        if (bool2 === false) {
+          bool1 = '';
+        }
+        foundInSecond.push(['', key, bool2]);
       }
     }
     return [...foundInBoth, ...foundInFirst, ...foundInSecond];

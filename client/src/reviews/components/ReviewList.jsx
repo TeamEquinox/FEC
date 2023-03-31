af7cdad3f210f2aca95df53df44805049e64c0f1
George Halterman <george.h881@gmail.com>
2023-03-31 14:41:21 -0700

    saving before pulling from main

--- client/src/reviews/components/ReviewList.jsx
+++ client/src/reviews/components/ReviewList.jsx
@@ -20,6 +20,7 @@ function ReviewList({
   prodCharacteristics,
   prodName,
 }) {
+  const [helpfulReviews, setHelpfulReviews] = useState([]);
   const [reviewCount, setReviewCount] = useState(2);
   const [showModal, setShowModal] = useState(false);
   const [tempCharStorage, setTempCharStorage] = useState({});
@@ -68,15 +69,26 @@ function ReviewList({
     setReviewCount(reviewCount + 2);
   };
 
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   const helpfulReviewHandler = (reviewId) => {
-    putHelpfulReview(reviewId)
-      .then(() => {
-        console.log('Success from putHelpfulReview');
-        setToggle(!toggle);
-      })
-      .catch((err) => {
-        console.log(`error with ${reviewId} in putHelpfulReview`, err);
-      });
+    if (!helpfulReviews.includes(reviewId)) {
+      putHelpfulReview(reviewId)
+        .then(() => {
+          console.log('Success from putHelpfulReview');
+          setToggle(!toggle);
+          setHelpfulReviews([...helpfulReviews, reviewId]);
+        })
+        .catch((err) => {
+          console.log(`error with ${reviewId} in putHelpfulReview`, err);
+        });
+    }
   };
 
   const reportReview = (reviewId) => {
@@ -88,14 +100,6 @@ function ReviewList({
       .catch((err) => {
         console.log(`error with ${reviewId} in reportReview`, err);
       });
-  };
-
-  const handleShowModal = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
   };
 
   const handleSubmitReview = (event) => {
